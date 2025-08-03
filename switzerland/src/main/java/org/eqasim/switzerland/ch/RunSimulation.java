ce16c9ef7d55f226e3bdef1b06a2c283b4fdf6b6
shilangyu <xmarcinmarcin@gmail.com>
2025-08-03 12:11:53 +0200

    Add sharing module

--- switzerland/src/main/java/org/eqasim/switzerland/ch/RunSimulation.java
+++ switzerland/src/main/java/org/eqasim/switzerland/ch/RunSimulation.java
@@ -1,6 +1,9 @@
 package org.eqasim.switzerland.ch;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.CommandLine;
@@ -10,6 +13,27 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.contrib.sharing.run.SharingConfigGroup;
+import org.matsim.contrib.sharing.run.SharingModule;
+import org.matsim.contrib.sharing.run.SharingServiceConfigGroup;
+import org.matsim.contrib.sharing.run.SharingServiceConfigGroup.ServiceScheme;
+import org.matsim.contrib.sharing.service.SharingUtils;
+import org.matsim.core.config.CommandLine;
+import org.matsim.core.config.CommandLine.ConfigurationException;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.ScoringConfigGroup.ActivityParams;
+import org.matsim.core.config.groups.ScoringConfigGroup.ModeParams;
+import org.matsim.core.config.groups.RoutingConfigGroup.ModeRoutingParams;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.config.groups.ScoringConfigGroup.ActivityParams;
+import org.matsim.core.config.groups.ScoringConfigGroup.ModeParams;
+import org.matsim.core.config.groups.RoutingConfigGroup.ModeRoutingParams;
+import org.matsim.contrib.sharing.run.SharingConfigGroup;
+import org.matsim.contrib.sharing.run.SharingModule;
+import org.matsim.contrib.sharing.run.SharingServiceConfigGroup;
+import org.matsim.contrib.sharing.run.SharingServiceConfigGroup.ServiceScheme;
+import org.matsim.contrib.sharing.service.SharingUtils;
 
 import ch.sbb.matsim.mobsim.qsim.SBBTransitModule;
 import ch.sbb.matsim.mobsim.qsim.pt.SBBTransitEngineQSimModule;
@@ -28,6 +52,64 @@ public class RunSimulation {
 		configurator.updateConfig(config);
 		cmd.applyConfiguration(config);
 
+		// We define bike to be routed based on Euclidean distance.
+		ModeRoutingParams bikeRoutingParams = new ModeRoutingParams("bike");
+		bikeRoutingParams.setTeleportedModeSpeed(5.0);
+		bikeRoutingParams.setBeelineDistanceFactor(1.3);
+		config.routing().addModeRoutingParams(bikeRoutingParams);
+
+		// Walk is deleted by adding bike here, we need to re-add it ...
+		ModeRoutingParams walkRoutingParams = new ModeRoutingParams("walk");
+		walkRoutingParams.setTeleportedModeSpeed(2.0);
+		walkRoutingParams.setBeelineDistanceFactor(1.3);
+		config.routing().addModeRoutingParams(walkRoutingParams);
+
+		SharingConfigGroup sharingConfig = new SharingConfigGroup();
+		config.addModule(sharingConfig);
+
+		// We need to define a service ...
+		SharingServiceConfigGroup serviceConfig = new SharingServiceConfigGroup();
+		sharingConfig.addService(serviceConfig);
+
+		// ... with a service id. The respective mode will be "sharing:velib".
+		serviceConfig.setId("velib");
+
+		// ... with freefloating characteristics
+		serviceConfig.setMaximumAccessEgressDistance(100000);
+		serviceConfig.setServiceScheme(ServiceScheme.StationBased);
+		serviceConfig.setServiceAreaShapeFile(null);
+
+		// ... with a number of available vehicles and their initial locations
+		// the following file is an example and it works with the siouxfalls-2014 scenario
+		serviceConfig.setServiceInputFile("shared_taxi_vehicles_stations.xml");
+
+		// ... and, we need to define the underlying mode, here "bike".
+		serviceConfig.setMode("bike");
+
+		// Finally, we need to make sure that the service mode (sharing:velib) is
+		// considered in mode choice.
+		List<String> modes = new ArrayList<>(Arrays.asList(config.subtourModeChoice().getModes()));
+		modes.add(SharingUtils.getServiceMode(serviceConfig));
+		config.subtourModeChoice().setModes(modes.toArray(new String[modes.size()]));
+
+		// We need to add interaction activity types to scoring
+		ActivityParams pickupParams = new ActivityParams(SharingUtils.PICKUP_ACTIVITY);
+		pickupParams.setScoringThisActivityAtAll(false);
+		config.scoring().addActivityParams(pickupParams);
+
+		ActivityParams dropoffParams = new ActivityParams(SharingUtils.DROPOFF_ACTIVITY);
+		dropoffParams.setScoringThisActivityAtAll(false);
+		config.scoring().addActivityParams(dropoffParams);
+
+		ActivityParams bookingParams = new ActivityParams(SharingUtils.BOOKING_ACTIVITY);
+		bookingParams.setScoringThisActivityAtAll(false);
+		config.scoring().addActivityParams(bookingParams);
+
+		// We need to score bike
+		ModeParams bikeScoringParams = new ModeParams("bike");
+		config.scoring().addModeParams(bikeScoringParams);
+
+
 		if (cmd.hasOption("preventwaitingtoentertraffic")) {
 			if (cmd.getOption("preventwaitingtoentertraffic").get().equals("y")) {
 				((QSimConfigGroup) config.getModules().get(QSimConfigGroup.GROUP_NAME))
@@ -45,14 +127,16 @@ public class RunSimulation {
 		configurator.configureController(controller);
         controller.addOverridingModule(new PTPassengerCountsModule());
         controller.addOverridingModule(new PTLinkVolumesModule());
+		controller.addOverridingModule(new SharingModule());
 
 		 // To use the deterministic pt simulation (Part 1 of 2):
         controller.addOverridingModule(new SBBTransitModule());
         // To use the deterministic pt simulation (Part 2 of 2):
         controller.configureQSimComponents(components -> {
             new SBBTransitEngineQSimModule().configure(components);
-
+			SharingUtils.configureQSim(sharingConfig).call(components);
         });
+
 		controller.run();
-	}
+	}§
 }
